7c2ca914019e031d853a662a48b51ddf7271c10d
Edris Berg <ali_berg1@yahoo.com>
2021-09-14 20:35:14 -0700

    styled nav bar

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ alert('Hi, ' + userName + " welcome to my About Me website!");
 let school = prompt('Please, answer by a yes or no. Do I have a B.S in IT?');
 if (school.toUpperCase() == 'YES' || school.toUpperCase() == 'Y') {
     alert('You got that right!');
-    console.log('You got that right, I have B.S in IT!');
+    //console.log('You got that right, I have B.S in IT!');
 } else {
     alert('Sorry, I do have a B.S in IT');
 }
@@ -14,7 +14,7 @@ if (school.toUpperCase() == 'YES' || school.toUpperCase() == 'Y') {
 let intern = prompt('Please answer by a yes or no. I interned at Yale Health?');
 if (intern.toUpperCase() == 'YES' || intern.toUpperCase() == 'Y') {
     alert('You got that right!');
-    console.log('You got that right, I interned at Yale Health!');
+    //console.log('You got that right, I interned at Yale Health!');
 } else {
     alert('Sorry, I did intern at Yale');
 }
@@ -22,7 +22,7 @@ if (intern.toUpperCase() == 'YES' || intern.toUpperCase() == 'Y') {
 let restaurant = prompt('Please, answer by a yes or no. Did I ever work at a restaurant?');
 if (restaurant.toUpperCase() == 'YES' || restaurant.toUpperCase() == 'Y') {
     alert('You got that right!');
-    console.log('You got that right, I worked at a restaurant!');
+    //console.log('You got that right, I worked at a restaurant!');
 } else {
     alert('Sorry, I did work at a restaurant');
 }
@@ -30,7 +30,7 @@ if (restaurant.toUpperCase() == 'YES' || restaurant.toUpperCase() == 'Y') {
 let major = prompt('Please answer by a yes or no. Am I studying to become Full-Stack Dev?');
 if (major.toUpperCase() == 'YES' || major.toUpperCase() == 'Y') {
     alert('You got that right!');
-    console.log('You got that right, I am studying to become Full-Stack!');
+    //console.log('You got that right, I am studying to become Full-Stack!');
 } else {
     alert('Sorry, I am actually studying to become a Full-Stack Dev');
 }
@@ -38,8 +38,7 @@ if (major.toUpperCase() == 'YES' || major.toUpperCase() == 'Y') {
 let interest = prompt('Please answer by a yes or no. Do I like Soccer?');
 if (interest.toUpperCase() == 'YES' || interest.toUpperCase() == 'Y') {
     alert('You got that right!');
-    console.log('You got that right!');
-    console.log('You got that right, I love soccer!');
+    //console.log('You got that right, I love soccer!');
 } else {
     alert('Sorry, I actually do like soccer');
 }
