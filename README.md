a4bc1bdf38a13e102f2a6642b1075782b8e1ef0a
Edris Berg <ali_berg1@yahoo.com>
2021-09-15 21:19:58 -0700

    added new functionality to the project

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # About-Me
 A website to introduce myself and get people to know me.
+
+## 9/14/2021
+Implented the site using HTML, basic CSS, and JS using prompt, alert and console.log commands.
+- Asking user to enter his/her name and to answer 5 questions about me by a yes/no.
+- Greet the user and display whether the answers were correct or wrong.
+- Thank you user at the end.
+
+## 9/15/2021
+### Added some functionality and more data including:
+1.  Added an ordered list of Top Ten places to visit.
+2.  Converted my work experience and education into unordered list.
+3.  Added a 6th question that asks the user to guess a number
+    - Added a loop to allow the user to make 4 guesses and letting the user know whether the guess was to high/low. And finally telling the user the correct number.
+4. Added a 7th question that has multiple correct answers stored using an array.
+   - Using a loop given the user 6 attempts to guess. ending if the guess was correct, otherwise display the correct answers once the user exhausted all attempts.
+
+5. Made a counter to track the user score to display the final score at the end.
+
--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-'use strict';
-
+// 'use strict';
+let correctAnswers = 0;
 let userName = prompt('What is your name?');
 alert('Hi, ' + userName + " welcome to my About Me website!");
 
 let school = prompt('Please, answer by a yes or no. Do I have a B.S in IT?');
 if (school.toUpperCase() == 'YES' || school.toUpperCase() == 'Y') {
     alert('You got that right!');
+    correctAnswers++;
     //console.log('You got that right, I have B.S in IT!');
 } else {
     alert('Sorry, I do have a B.S in IT');
@@ -14,6 +15,7 @@ if (school.toUpperCase() == 'YES' || school.toUpperCase() == 'Y') {
 let intern = prompt('Please answer by a yes or no. I interned at Yale Health?');
 if (intern.toUpperCase() == 'YES' || intern.toUpperCase() == 'Y') {
     alert('You got that right!');
+    correctAnswers++;
     //console.log('You got that right, I interned at Yale Health!');
 } else {
     alert('Sorry, I did intern at Yale');
@@ -22,6 +24,7 @@ if (intern.toUpperCase() == 'YES' || intern.toUpperCase() == 'Y') {
 let restaurant = prompt('Please, answer by a yes or no. Did I ever work at a restaurant?');
 if (restaurant.toUpperCase() == 'YES' || restaurant.toUpperCase() == 'Y') {
     alert('You got that right!');
+    correctAnswers++;
     //console.log('You got that right, I worked at a restaurant!');
 } else {
     alert('Sorry, I did work at a restaurant');
@@ -30,6 +33,7 @@ if (restaurant.toUpperCase() == 'YES' || restaurant.toUpperCase() == 'Y') {
 let major = prompt('Please answer by a yes or no. Am I studying to become Full-Stack Dev?');
 if (major.toUpperCase() == 'YES' || major.toUpperCase() == 'Y') {
     alert('You got that right!');
+    correctAnswers++;
     //console.log('You got that right, I am studying to become Full-Stack!');
 } else {
     alert('Sorry, I am actually studying to become a Full-Stack Dev');
@@ -38,9 +42,75 @@ if (major.toUpperCase() == 'YES' || major.toUpperCase() == 'Y') {
 let interest = prompt('Please answer by a yes or no. Do I like Soccer?');
 if (interest.toUpperCase() == 'YES' || interest.toUpperCase() == 'Y') {
     alert('You got that right!');
+    correctAnswers++;
     //console.log('You got that right, I love soccer!');
 } else {
     alert('Sorry, I actually do like soccer');
 }
 
-alert(userName + ', thank you for taking the time and asnwering the questions!');
+//guessing number
+let guess = prompt("Guess a number 1 to 100. You have 4 attempts to guess the number!");
+let numOfGuess = 4;
+let number = Math.floor(Math.random() * 100) + 1;
+console.log(number);
+for (let attempt = 1; attempt <= numOfGuess; attempt++) {
+
+    if (guess == number) {
+        alert("Good job, your guess is correct!");
+        correctAnswers++;
+        break;
+
+    } else if (guess < number) {
+        alert('Your guess is too low. You have ' + (numOfGuess - attempt) + ' attempts left, guess again!');
+        guess = prompt('Guess a number 1 to 100. you have ');
+
+    } else if (guess > number) {
+        alert('Your guess is too high. You have ' + (numOfGuess - attempt) + ' attempts left, guess again!');
+        guess = prompt('Guess a number 1 to 100. you have ');
+    }
+
+    else {
+        alert('Sorry, you run out of attempts!');
+        break;
+    }
+    console.log(numOfGuess);
+}
+
+
+// guessing my top ten countries
+let topTenCountires = ['Turkey', 'Morocco', 'Spain', 'Thailand', 'Italy', 'Sweden', 'Peru', 'China', 'Pakistan', 'Jordan'];
+
+
+let guesses = 0;
+while (guesses < 6) {
+    let yourGuess = prompt('You have 6 attempts to guess my top ten countries. Please, enter a country name');
+    // create a for loop based on the answers. If the guess is within the array break.
+    // make guesses var = 6 to break the while loop.
+    for (let i = 0; i < topTenCountires.length - 1; i++) {
+        if (yourGuess.toLowerCase() == topTenCountires[i].toLowerCase()) {
+            console.log('You got that right! ' + yourGuess + ' Is one of my top countries to visit');
+            correctAnswers++;
+            guesses = 10;
+            break;
+        }
+    }
+    guesses++;
+
+    // }
+
+    //console.log(guesses);
+
+}
+
+if (guesses == 6) {
+    console.log('Sorry, you run out of attempts. Below are the correct answers.');
+    for (let country = 0; country < topTenCountires.length; country++) {
+        alert(topTenCountires[country]);
+       // console.log(topTenCountires[country]);
+    }
+}
+
+//console.log(correctAnswers);
+console.log(userName + ', thank you for taking the time and asnwering the questions. You scored ' + correctAnswers + ' out of 7 questions');
+
+alert(userName + ', thank you for taking the time and asnwering the questions. You scored ' + correctAnswers + ' out of 7 questions');
